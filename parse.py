574c6662aed309325bd7bedcd825a5c5983f3c02
Ben Pettis <bpettis@wisc.edu>
2022-04-18 16:24:15 -0500

    output HTTP response headers and bodies

--- parse.py
+++ parse.py
@@ -7,27 +7,44 @@ with open(filename) as input_file:
 	bulk_data = json.load(input_file)
 
 for host in bulk_data:
-	print(bulk_data[host])
+   
+	print(bulk_data[host]['ip'])
+	print(bulk_data[host]['last_updated_at'])
+    
 	for service in bulk_data[host]['services']:
-		print(service['_decoded'])
-		if('http' in service):
-		#if(service['http']):
-			if (service['http']['response']['status_code'] == '451'):
-				try:
-					print(service['http']['response']['status_code'])
-				except:
-					print('could not get status code')
-				try:
-					print(service['http']['response']['headers'])
-				except:
-					print('could not get response header')
+		try:
+			if(service['http']['response']['status_code'] == 451):
+
+				#Print HTTP headers prettier:				
+				for item in service['http']['response']['headers']:
+					try:
+						print(str(item) + ': ' + str(service['http']['response']['headers'][item][0]))
+					except:
+						print('')
 				print('\n')
-				try:
-					print(service['http']['response']['body'])
-				except:
-					print('could not get response body')
+				print(service['http']['response']['body'])
+				print('\n')
+				
+				response_filename = 'output/responses/' + str(bulk_data[host]['last_updated_at']) + '-' + str(bulk_data[host]['ip']) + '.txt'
+				html_filename = 'output/responses/' + str(bulk_data[host]['last_updated_at']) + '-' + str(bulk_data[host]['ip']) + '.html'
+				with open(response_filename, 'w') as outfile:
+					for item in service['http']['response']['headers']:
+						try:
+							outfile.write(str(item) + ': ' + str(service['http']['response']['headers'][item][0]) + '\n')
+						except:
+							print('')
+					outfile.write('\n\n')
+					outfile.write(str(service['http']['response']['body']))
+				
+				with open(html_filename, 'w') as html:
+					html.write(str(service['http']['response']['body']))
+					
 			else:
-				print('Not a 451...')
-		else:
-			print('Skipping...')
+				print('not 451')
+			
+		except:
+			print('failed - likely not http\n')
+			
+
+
 	print('*** *** *** *** ***\n')
