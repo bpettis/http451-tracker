a56a41b79193827cdd595611563ce7262d25f4a1
Ben Pettis <bpettis@wisc.edu>
2022-04-22 21:32:11 -0500

    create basic files to test cloud run container

---
+++ web-frontend/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	</head>
+	
+	<body>
+		<h1>Hello world!</h1>
+		<h2><?php echo "It works!" ?>
+		<p>“What is there about fire that’s so lovely? No matter what age we are, what draws us to it?’ Beatty blew out the flame and lit it again. ‘It’s perpetual motion; the thing man wanted to invent but never did.” </p>
+	</body>
+</html>
