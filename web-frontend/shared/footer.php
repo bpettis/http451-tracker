30962733ebca8cfa976835cce8d24ed5581d3c5e
Ben Pettis <bpettis@wisc.edu>
2022-12-15 21:25:10 -0600

    add donate link

--- web-frontend/shared/footer.php
+++ web-frontend/shared/footer.php
@@ -1,8 +1,10 @@
 <footer class="mt-auto">
 	Host scan data is Copyright &copy; <?php echo date("Y"); ?> <a href="https://censys.io">Censys</a> <br />
 	Website design and data analysis <span class="copy-left">&copy;</span> 2022 - <?php echo date("Y"); ?> Ben Pettis - Some Rights Reserved <br />
+	Enjoy this? <a href="https://ko-fi.com/bpettis">Buy me a Ko-Fi!</a> <br />
 	Except where otherwise noted, this work is released under a Creative Commons License <br />
 	<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">(CC-BY-NC-SA 4.0)</a>
+	
 	
 	<!-- Bootstrap core JS-->
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
